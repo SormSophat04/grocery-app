a599cace20c77f08df93e4297b2005d6f3bddbd1
Sorm Sophat <sormsophat@lolc.com.kh>
2026-01-12 17:04:21 +0700

    create file splash and color

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  const AppColor._();
+  static const backgroundWhite = 0xFFFFFFFF;
+  static const backgroundSecondary = 0xFFF4F5F9;
+  static const backgroundEnd = 0xffF5F5F5;
+  static const textGray = Colors.grey;
+  static const textBlack = Colors.black;
+  static const link = 0xff1A0DAB;
+  static const border = 0xffEBEBEB;
+  static const primary = 0xffAEDC81;
+  static const primaryDark = 0xff6CC51D;
+  static const primaryLight = 0xffEBFFD7;
+}
